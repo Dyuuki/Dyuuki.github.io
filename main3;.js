e9b941ad23d2592e03775cbfc2d07eba70c5c767
Dyuuki <97532941+Dyuuki@users.noreply.github.com>
2022-07-20 20:56:42 +0900

    最終課題

---
+++ main3;.js
@@ -0,0 +1,13 @@
+function calc(){
+var zikan=document.getElementById("zikan");
+
+document.getElementById("result").value=zikan.valueAsNumber/864000
+}
+
+var style=document.createElement("style");
+document.head.appendChild(style);
+
+var setVal=(document.getElementById("result").value);
+
+const textbox=document.getElementById("content")
+const value=textbox.value
